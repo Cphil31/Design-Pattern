3309d8bd74fab9e1d8102270daa92d35190f161b
cphil31 <cecile_philippe@live.fr>
2016-10-19 13:38:25 +0200

    fonction reset en cours

--- object.js
+++ object.js
@@ -2,70 +2,68 @@
 (function(){
   // object
   window.app = {
-  	timer : 10,
-
-  	intervalID:null,
-  	init: function(){
-  		app.listeners();
-
+  	
+    timer:20,
+   
+    intervalID:null,
+    init: function(){
+      app.listeners();
   	},
+
   	listeners:function(){
   		$("#start").click(app.start);
   		$("#stop").on('click', app.stop);
-  		$("#reset").on('click', app.recommencer);
+  		$("#reset").on('click', app.reset);
   	},
 
 
   	start:function(){
-  	
-  		app.intervalID=setInterval(app.decremente,1000);
-  	},
+      app.stop();
+      app.intervalID=setInterval(app.decremente,1000);
 
-  	decremente : function () {
-  		app.timer--;
-  		app.afficher();
-  		if(app.timer==0){
-  			clearInterval(app.intervalID);
-  		}
-  	},
+    },
 
-  	stop:function(){
-  		clearInterval(app.intervalID);
+    decremente : function () {
+      app.timer--;
+      app.afficher();
+      if(app.timer<=0){
+       clearInterval(app.intervalID);
 
-  	},
-  	afficher:function(){
-  		var minute = parseInt(app.timer/60, 10);
-  		var seconde = parseInt(app.timer - minute*60);
+     }
+    },
 
+   stop:function(){
+    clearInterval(app.intervalID);
 
-  		if(minute < 10){
-			minute = '0'+ minute; 
-		}
-		if(seconde < 10){
-			seconde = '0' + seconde;
-		}
-  		$("#minute").html(minute);
-  		$("#seconde").html(seconde);
-  	},
+  },
+  afficher:function(){
 
-  	recuperer:function(){
-		$("#inputMinutes").val(),
-		$("#inputSecondes").val();
-	
-  	},
-
-  	recommencer:function(){
-  		
-		app.stop();
-		app.timer=
-		app.start();
-		
-
-  	},
+    var minute = parseInt(app.timer/60, 10);
+    var seconde = parseInt(app.timer - minute*60);
 
 
-  };
+    if(minute < 10){
+     minute = '0'+ minute; 
+   }
+   if(seconde < 10){
+     seconde = '0' + seconde;
+   }
+   $("#minute").html(minute);
+   $("#seconde").html(seconde);
 
-  app.init();
+ },
+
+ recuperer:function(){
+  var sec =$("#inputSecondes").val();
+  var min =$("#inputMinutes").val();
+  },
+
+reset:function(){
+  return app.start();
+  
+},
+};
+
+app.init();
 
 })();
